2b5f8378bc99ab2543652d04d1584885c48a5f45
Neue Károly <neuekaroly@INF.ELTE.HU>
2018-02-27 11:47:39 +0100

    Finish gyak 3.

--- adat2_03.sql
+++ adat2_03.sql
@@ -1,1 +1,32 @@
-s
+--dba_synonyms
+select * from dba_synonyms
+
+--synonym nikovits.szeret táblára
+create synonym nsz for nikovits.szeret
+
+--saját synonymok lekérés
+select * from user_synonyms
+
+--view készítése
+create or replace view nevsor as select nev from nikovits.szeret;
+
+--táblázat oszlopainak kiírása
+desc szeret
+
+--sequence
+create sequence szamok 
+start with 20
+increment by 5
+nocycle
+nocache;
+
+--kezdőértéket ad
+select szamok.nextval from dual
+
+--jelenlegi érték
+select szamok.currval from dual
+
+--data base link
+create database link ullman connect to rb653s identified by rb653s using 'ullman.inf.elte.hu:1521/ullman';
+
+--data base link hivatkozas
